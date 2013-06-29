e29dd0c44f68a29d6769cc3ea168780551b86f50
Brian Gonzalez <brian.gonzalez@geni.com>
2013-06-28 21:26:45 -0700

    edit hook

--- lib/pre-push.sh
+++ lib/pre-push.sh
@@ -8,23 +8,16 @@ echo "Running pre-push hook on: " $NAME
 # don't run on rebase
 if [ $NAME != '(no branch)' ]
 then
-  git stash -q --keep-index
+  # git stash -q --keep-index
   grunt
 
   RETVAL=$?
 
-  if [ $RETVAL -ne 0 ]
-  then
+  if [ $RETVAL -ne 0 ] then
     exit 1
   fi
 
   git add .
-  git stash pop -q
-
-  if [ $RETVAL -ne 0 ]
-    echo "Popped staged changes."
-  then
-    echo "Nothing to pop."
-  fi  
+  # git stash pop -q
 
 fi
